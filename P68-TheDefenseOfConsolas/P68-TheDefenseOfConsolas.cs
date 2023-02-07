70b21de0f03d56d3b03896c5de7978de82ab4b76
doomydot <alice@somnolentgames.com>
2023-02-08 00:32:09 +0100

    The Defense of Consolas

---
+++ P68-TheDefenseOfConsolas/P68-TheDefenseOfConsolas.cs
@@ -0,0 +1,22 @@
+﻿int targetRow;
+int targetColumn;
+
+Console.Title = "--- The Defense of Consolas ---";
+Console.WriteLine("--- The Defense of Consolas ---");
+
+Console.Write("Target column: ");
+targetColumn = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target row: ");
+targetRow = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Deploy to: ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"       ({targetColumn} , {targetRow+1})    ");
+Console.Write($"({targetColumn - 1} , {targetRow})");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("   x   ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"{targetColumn + 1} , {targetRow})");
+Console.WriteLine($"       ({targetColumn} , {targetRow-1})");
+
+Console.Beep();
