da6c8ca04f1f26e5dbb59bb67c4d93231e0af584
doomydot <alice@somnolentgames.com>
2023-02-09 02:22:38 +0100

    p75-78

---
+++ P75-RepairingTheClocktower/P75-RepairingTheClocktower.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter a number: ");
+var number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number % 2 == 0 ? "Tick" : "Tock");
---
+++ P78-Watchtower/P78-Watchtower.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Watchtower");
+
+Console.Write("Enter x: ");
+var x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y: ");
+var y = Convert.ToInt32(Console.ReadLine());
+
+string direction = "";
+
+if (y < 0)      direction += "south";
+else if (y > 0) direction += "north";
+
+if (x < 0)      direction += "west";
+else if (x > 0) direction += "east";
+
+
+if (!(x == 0 && y == 0)){
+    Console.WriteLine($"The enemy is to the {direction}!");
+}
+else {
+    Console.WriteLine("The enemy is upon us!");
+}
