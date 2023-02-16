6c96469d389722898a674070956bc55f523c456e
doomydot <olof.hagstrom@gmail.com>
2023-02-16 13:23:49 +0100

    P89

---
+++ P89-TheMagicCannon/P89-TheMagicCannon.cs
@@ -0,0 +1,8 @@
+﻿for (int i = 1; i <= 100; i++) {
+    var projectileType = "Normal";
+    if (i % 3 == 0)
+        projectileType = "Fire";
+    if (i % 5 == 0)
+        projectileType = "Electric";
+    Console.WriteLine($"{i}. {projectileType}");
+}
