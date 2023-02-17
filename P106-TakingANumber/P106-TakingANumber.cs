4b3ac6e86788e4736b5705cc9759b41586bb8399
doomydot <olof.hagstrom@gmail.com>
2023-02-17 09:01:56 +0100

    P95-P106

---
+++ P106-TakingANumber/P106-TakingANumber.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+
+
+
+int AskForNumber(string text) {
+    Console.WriteLine(text);
+    Console.Write("> ");
+    var response = Convert.ToInt32(Console.ReadLine());
+    return response;
+}
+
+int AskForNumbersInRange(string text, int min, int max) {
+    Console.WriteLine(text);
+    var response = Int32.MaxValue;
+    while (response ! > min && response ! < max) {
+        Console.Write("> ");
+        response = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return response;
+}
---
+++ P95-TheLawsOfFreach/P95-TheLawsOfFreach.cs
@@ -0,0 +1,12 @@
+﻿var array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+var currentSmallest = int.MaxValue;
+foreach (var t in array) {
+    currentSmallest = t;
+}
+Console.WriteLine(currentSmallest);
+
+var total = array.Sum();
+
+var average = (float)total / array.Length;
+Console.WriteLine(average);
