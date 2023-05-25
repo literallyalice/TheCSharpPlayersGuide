c85fe8abf17708edb19df9af8cda9980156be2f1
doomydot <alice@somnolentgames.com>
2023-05-25 13:53:41 +0200

    Up to p143

---
+++ P135-SimulasTest/P135-SimulasTest.cs
@@ -0,0 +1,36 @@
+﻿ChestState currentChestState = ChestState.Locked;
+
+Console.WriteLine("Simula's Test");
+Console.WriteLine("Commands: Open, Close, Lock, Unlock.");
+Console.WriteLine("------------------------------------");
+while (true) {
+    Console.WriteLine($"The chest is {currentChestState.ToString().ToLower()}. What do you want to do?");
+    Console.Write("> ");
+    var input = Console.ReadLine()?.ToLower();
+    switch (currentChestState) {
+        
+        case ChestState.Open:
+            if (input == "close") currentChestState++;
+            break;
+        
+        case ChestState.Locked:
+            if (input == "unlock") currentChestState--;
+            break;
+        
+        case ChestState.Closed:
+            if (input == "open") currentChestState--;
+            else if (input == "lock") currentChestState++;
+            break;
+        
+        default:
+            break;
+    }
+}
+
+
+enum ChestState
+{
+    Open = 0,
+    Closed = 1,
+    Locked = 2
+}
---
+++ P143-SimulasSoup/P143-SimulasSoup.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("--- Simula's Soup ---");
+(SoupType soupType, Ingredient ingredient, Seasoning seasoning) Soup;
+
+
+Console.WriteLine("-- Type of Soups --");
+Soup.soupType = (SoupType)ChooseFromList(Enum.GetNames(typeof(SoupType)));
+Console.WriteLine("-- Main Ingredients --");
+Soup.ingredient = (Ingredient)ChooseFromList(Enum.GetNames(typeof(Ingredient)));
+Console.WriteLine("-- Seasoning --");
+Soup.seasoning = (Seasoning)ChooseFromList(Enum.GetNames(typeof(Seasoning)));
+
+Console.WriteLine("You've ordered a " + Soup.seasoning + " " + Soup.soupType + " with " + Soup.ingredient + ".");
+
+
+int ChooseFromList(string[] names) {
+    var i = 0;
+    foreach (string label in names) {
+        i++;
+        Console.WriteLine(i + ". " + label);
+    }
+    
+    Console.Write("Enter a number: (#) ");
+    string? input = Console.ReadLine();
+    int number;
+    while (!int.TryParse(input, out number) &&
+           Convert.ToInt32(input) < names.Length &&
+           Convert.ToInt32(input) > 0) {
+        Console.WriteLine("Invalid input, please enter a number!");
+        Console.Write("Enter a number: (#) ");
+        input = Console.ReadLine();
+    }
+    Console.WriteLine("You selected " + names[number - 1] + ".");
+
+    return number;
+}
+
+enum SoupType
+{
+    Soup = 1,
+    Stew,
+    Gumbo
+}
+
+enum Ingredient
+{
+    Mushroom = 1,
+    Chicken,
+    Carrots,
+    Potatoes
+}
+
+enum Seasoning
+{
+    Spicy = 1,
+    Salty,
+    Sweet
+}
