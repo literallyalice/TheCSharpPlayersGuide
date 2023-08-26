15e5e6f24e2b7bb6fcacebd9ec80889ff60db16b
doomydot <olof.hagstrom@gmail.com>
2023-08-26 19:07:26 +0200

    RockPaperScissors

---
+++ P193-RockPaperScissorCRC/P193-RockPaperScissorCRC.cs
@@ -0,0 +1,28 @@
+﻿/*
+
+GameManager:
+Collab: Player, Round
+initiate rounds until game is closed
+Keep track of rounds
+
+
+
+Round:
+Collab: Player
+Track state of a single round
+decide winner
+display round result
+
+Player:
+Collab:
+Get input from user
+
+
+
+
+
+
+
+
+
+*/
