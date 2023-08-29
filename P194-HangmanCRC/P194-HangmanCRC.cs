2f65ec9f1ef078ab04b4013b12e328efb42a971b
doomydot <olof.hagstrom@gmail.com>
2023-08-29 12:07:46 +0200

    TicTacToe progress

---
+++ P194-HangmanCRC/P194-HangmanCRC.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+
+/*
+
+GameManager.cs
+Collaborators: Player, Dictionary
+- Keeps track of the games word
+- creates new char array as long as the games word
+- Keeps track of incorrect guesses
+- Keeps track of remaining guesses
+- Get move
+- Decide if win or draw
+
+Player.cs
+Collaborators: GameManager
+- Takes input from player
+- checks if input is valid
+- displays the state of the game
+
+Dictionary.cs
+- Keeps a list of words
+- Can pick a word at random
+
+
+
+
+*/
---
+++ P195-TicTacToe/P195-TicTacToe.cs
@@ -0,0 +1,40 @@
+﻿
+GameManager game = new GameManager();
+
+
+// Collaborators: PlayerInterface
+public class GameManager
+{
+    public bool XTurn { get; set; }
+    public int[] Board { get; set; }
+    public PlayerInterface Interface;
+    
+
+    public GameManager() {
+        XTurn = true;
+        Board = new int[9];
+        Array.Clear(Board, 0, Board.Length);
+
+        Interface = new PlayerInterface();
+    }
+
+    public void StartGame() {
+        Interface.RenderBoard(Board, XTurn);
+    }
+    
+    
+}
+
+// Collaborators: GameManager
+public class PlayerInterface
+{
+    public void RenderBoard(int[] board, bool xTurn) {
+        Console.WriteLine(xTurn ? "It's X's Turn." : "It's O's turn.");
+        Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine($" {board[0]} | {board[1]} | {board[2]} ");
+        Console.WriteLine("Select square with ");
+    }
+}
