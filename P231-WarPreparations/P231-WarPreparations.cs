adf96feb328e9bca1d7e8bd17cf3eaa3635a751d
doomydot <olof.hagstrom@gmail.com>
2024-01-07 07:06:51 +0100

    up to 240

---
+++ P231-WarPreparations/P231-WarPreparations.cs
@@ -0,0 +1,29 @@
+﻿
+var basicSword = new Sword(SwordType.Iron, GemType.None, 1.6f, 0.1f);
+var serpentBreath = basicSword with { Gem = GemType.Amber, Type = SwordType.Steel };
+var sapphiroth = basicSword with { Gem = GemType.Sapphire, Type = SwordType.Binarium };
+
+Console.WriteLine(basicSword);
+Console.WriteLine(serpentBreath);
+Console.WriteLine(sapphiroth);
+
+record Sword(SwordType Type, GemType Gem, float Length, float Width);
+
+enum SwordType
+{
+    Wood,
+    Bronze,
+    Iron,
+    Steel,
+    Binarium
+}
+
+enum GemType
+{
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone,
+    None
+}
