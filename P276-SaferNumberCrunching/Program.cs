47e712613dcc654ea4bc7a174681bc4df736f003
doomydot <olof.hagstrom@gmail.com>
2024-02-29 17:58:22 +0100

    idk

---
+++ P276-SaferNumberCrunching/Program.cs
@@ -0,0 +1,21 @@
+﻿int number;
+
+do {
+    Console.Write("Enter an integer: ");
+}
+while (int.TryParse(Console.ReadLine(), out number));
+Console.WriteLine(number);
+
+double number2;
+do {
+    Console.Write("Enter a double: ");
+}
+while (double.TryParse(Console.ReadLine(), out number2));
+Console.WriteLine(number2);
+
+bool truth;
+do {
+    Console.WriteLine("Enter true or false: ");
+}
+while (bool.TryParse(Console.ReadLine(),out truth));
+Console.WriteLine(truth);
