1e2ce10e92b921a5ec9c1724dc5eac16629d6639
doomydot <alice@somnolentgames.com>
2024-03-01 17:07:02 +0100

    betterrandom

---
+++ P278-BetterRandom/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+public static class RandomExtensions
+{
+    public static double NextDouble(this Random random, double max) {
+        return random.NextDouble() * max;
+    }
+
+    public static string NextString(this Random random, params string[] input) {
+        return input[random.Next(0, input.Length)];
+    }
+
+    public static bool CoinFlip(this Random random, float headsChance = 0.5f) {
+        return random.NextDouble() < headsChance;
+    }
+}
