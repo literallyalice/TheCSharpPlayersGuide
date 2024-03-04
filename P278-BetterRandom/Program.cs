ba2d06eb75ba038a06f1c7052987b3c616d681d7
doomydot <olof.hagstrom@gmail.com>
2024-03-04 10:26:51 +0100

    improved methods

--- P278-BetterRandom/Program.cs
+++ P278-BetterRandom/Program.cs
@@ -1,19 +1,20 @@
-﻿
+﻿var random = new Random();
 
-
-
+Console.WriteLine(random.NextDouble(100));
+Console.WriteLine(random.NextString("Knight", "Queen", "King"));
+Console.WriteLine(random.CoinFlip());
+Console.WriteLine(random.CoinFlip(0.25f));
 
 public static class RandomExtensions
 {
-    public static double NextDouble(this Random random, double max) {
-        return random.NextDouble() * max;
-    }
+    public static double NextDouble(this Random random, double max) 
+        => random.NextDouble() * max;
+    
 
-    public static string NextString(this Random random, params string[] input) {
-        return input[random.Next(0, input.Length)];
-    }
+    public static string NextString(this Random random, params string[] input) 
+        => input[random.Next(0, input.Length)];
+    
 
-    public static bool CoinFlip(this Random random, float headsChance = 0.5f) {
-        return random.NextDouble() < headsChance;
-    }
-}
+    public static bool CoinFlip(this Random random, float headsProbability = 0.5f) 
+        => random.NextDouble() < headsProbability;
+} 
