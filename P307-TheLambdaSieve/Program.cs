23e7b61313338b269aaf2d6843095cfd5e2c61cc
doomydot <alice@somnolentgames.com>
2024-03-22 16:20:38 +0100

    Lambda Sieve & The Long Game

---
+++ P307-TheLambdaSieve/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Welcome, select a sieve. (1. IsEven, 2. IsPositive, 3. IsMultipleOfTen)");
+Console.Write("> ");
+int.TryParse(Console.ReadLine(), out int input);
+
+var mySieve = input switch
+{
+    1 => new Sieve(n => n % 2 == 0),
+    2 => new Sieve(n => n > 0),
+    3 => new Sieve(n => n % 10 == 0),
+    _ => throw new ArgumentOutOfRangeException()
+};
+
+while (true) {
+    Console.WriteLine("Enter a number: ");
+    int.TryParse(Console.ReadLine(), out int numInput);
+
+    Console.WriteLine(mySieve.IsGood(numInput) ? $"{numInput} is good." : $"{numInput} is bad.");
+}
+
+class Sieve(Func<int, bool> input)
+{
+    public bool IsGood(int number) => input(number);
+}
---
+++ P315-TheLongGame/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("What's your Name?");
+Console.Write("> ");
+string name = Console.ReadLine();
+int score = 0;
+if (File.Exists($"{name}.txt")) {
+    score = int.Parse(File.ReadAllText($"{name}.txt"));
+}
+
+
+
+
+
+while (true) {
+    
+    Console.Write("> ");
+    ConsoleKey key = Console.ReadKey().Key;
+    if (key == ConsoleKey.Enter) {
+        File.WriteAllText($"{name}.txt", score.ToString());
+        break;
+    }
+    score++;
+    Console.WriteLine($"\nYour score is {score}.");
+}
+
+
