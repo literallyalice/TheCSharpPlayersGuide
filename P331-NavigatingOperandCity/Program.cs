9d45c5d5a71a975309a9a0a81d7467e886f066df
Alice Hagström <alice@somnolentgames.com>
2024-09-27 10:22:15 +0200

    NavigatingOperandCity

---
+++ P331-NavigatingOperandCity/Program.cs
@@ -0,0 +1,31 @@
+﻿BlockCoordinate block1 = new BlockCoordinate(0, 0);
+BlockOffset block2 = new BlockOffset(1, 1);
+block1 = block1 + Direction.South;
+Console.WriteLine($"{block1}");
+
+
+
+public record BlockCoordinate(int Row, int Column)
+{
+    public static BlockCoordinate operator +(BlockCoordinate start, BlockOffset offset) 
+        => new BlockCoordinate(start.Row + offset.RowOffset, start.Column + offset.ColumnOffset);
+
+    public static BlockCoordinate operator +(BlockCoordinate start, Direction dir)
+        => dir switch {
+            Direction.North => new BlockCoordinate(start.Row - 1, start.Column),
+            Direction.East => new BlockCoordinate(start.Row, start.Column + 1),
+            Direction.South => new BlockCoordinate(start.Row + 1, start.Column),
+            Direction.West => new BlockCoordinate(start.Row, start.Column - 1),
+            _ => new BlockCoordinate(start.Row, start.Column)
+        };
+};
+
+public record BlockOffset(int RowOffset, int ColumnOffset);
+
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West
+};
