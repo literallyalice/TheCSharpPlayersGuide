da25f8bf4e6737e2abf9a591a83b21fcc01d2ac8
Alice Hagström <alice.hagstrom@proton.me>
2025-01-30 09:35:29 +0100

    The Repeating Stream

--- P349-TheRepeatingStream/Program.cs
+++ P349-TheRepeatingStream/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿RecentNumbers recentNumbers = new();
+Thread thread = new(GenerateNumbers);
+thread.Start();
+while (true) {
+    Console.ReadKey();
+    if (recentNumbers.Numbers[0] == recentNumbers.Numbers[1]){
+        Console.WriteLine("> Repeating numbers detected!");
+    }
+    else {
+        Console.WriteLine("> No repeating numbers detected.");
+    }
+}
+void GenerateNumbers(){
+    while (true){
+        Random random = new Random();
+        int num = random.Next(0, 9);
+        Console.WriteLine("> Number generated: " + num);
+        recentNumbers.Add(num);
+        Thread.Sleep(1000);
+    }
+}
+
+class RecentNumbers {
+
+    private readonly object _lock = new object();
+    private List<int> _numbers = new List<int>();
+    public List<int> Numbers {
+
+        get{
+            lock(_lock){
+                return _numbers;
+            }
+        }
+    }
+    public void Add(int num){
+        lock(_lock){
+            _numbers.Add(num);
+            if (_numbers.Count > 2){
+                _numbers.RemoveAt(0);
+            }
+        }
+    }
+}
