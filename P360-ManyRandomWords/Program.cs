1bca54f3a90635ba690beeb81fc86165f8c9dd22
Alice Hagström <alice.hagstrom@proton.me>
2025-05-30 22:23:26 +0200

    ManyRandomWords + UniterOfAdds

---
+++ P360-ManyRandomWords/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+var input = "";
+    
+while (input != "q")
+{
+    Console.WriteLine("Enter a word: ");
+    Console.Write("> ");
+    input = Console.ReadLine()!.ToLower();
+    if (input == "q") break;
+
+    _ = HandleWord(input);
+
+}
+
+return;
+
+async Task HandleWord(string word)
+{
+    var timeBefore = DateTime.Now;
+    var tries = await RandomlyRecreateAsync(word);
+    var timeAfter = DateTime.Now - timeBefore;
+    
+    Console.WriteLine($"The word {word} took {tries} attempts and {timeAfter.TotalSeconds}s.");
+}
+
+int RandomlyRecreate(string word)
+{
+    StringBuilder randomWord = new(word.Length);
+
+    var tries = 0;
+    while (randomWord.ToString() != word)
+    {
+        randomWord.Clear();
+        for (var i = 0; i < randomWord.Capacity; i++)
+        {
+            randomWord.Append((char)('a' + Random.Shared.Next(26)));
+        }
+        tries++;
+    }
+    return tries;
+}
+
+Task<int> RandomlyRecreateAsync(string word)
+{
+    return Task.Run(() => RandomlyRecreate(word));
+}
---
+++ P365-UniterOfAdds/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine(Add(1, 2));
+Console.WriteLine(Add(1.02d, 1.03d));
+Console.WriteLine(Add("Hi ", "Dad"));
+var time = DateTime.Now;
+Console.WriteLine(Add(time, DateTime.Now - time));
+return;
+
+
+static dynamic Add(dynamic a, dynamic b) => a + b;
